c71f853440914a4ebc5ac4402466e6c320cffdfe
Bhautik2428 <122081508+Bhautik2428@users.noreply.github.com>
2023-09-19 19:41:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,38 +13,3 @@
 // console.log(myalgo(5 , 10));
 
 
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     const generateButton = document.getElementById("button_submit");
-//     generateButton.addEventListener("click", generateMultiples);
-// });
-
-// function generateMultiples() {
-//     const numInput = document.getElementById("num");
-//     const lengthInput = document.getElementById("length");
-//     const resultList = document.getElementById("result-list");
-//     const resultContainer = document.getElementById("result-container");
-
-//     const num = parseInt(numInput.value);
-//     const length = parseInt(lengthInput.value);
-
-//     if (isNaN(num) || isNaN(length)) {
-//         alert("Please enter valid numbers.");
-//         return;
-//     }
-
-//     const multiples = [];
-
-//     for (let i = 1; multiples.length < length; i++) {
-//         multiples.push(num * i);
-//     }
-
-//     // Display results
-//     multiples.forEach((multiple) => {
-//         const listItem = document.createElement("li");
-//         listItem.textContent = ` ${multiple}`;    // `${num} x ${multiples.indexOf(multiple) + 1} =
-//         resultList.appendChild(listItem);
-//     });
-
-//     resultContainer.style.display = "block";
-// }
